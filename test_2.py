09dab8157ac3c4d8ff87e1140fd9cdcae753ac02
Łukasz Świtalski <lukaz.switalski@gmail.com>
2023-09-28 19:53:49 +0200

    ui: add chooseing a file, display results,

--- test_2.py
+++ test_2.py
@@ -1,16 +1,35 @@
 from docx2python import docx2python
-from docx import Document
-import re
+from tkinter import Tk, Button, filedialog, Label
 
 def extract_questions(file_path):
     # Use docx2python to get the text with formatting
     with docx2python(file_path, html=True) as docx_content:
         text_with_formatting = docx_content.text
-    print(text_with_formatting)
+    result_label.config(text=text_with_formatting)
 
+def open_file_dialog():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        extract_questions(file_path)
 
-# Provide the path to the Word file
-word_file_path = "plik.docx"
+def clear_result():
+    result_label.config(text="")
 
-# Call the extraction function
-extract_questions(word_file_path)
+# Create the main Tkinter window
+root = Tk()
+root.title("Word Document Extractor")
+
+# Create a button to open the file dialog
+open_button = Button(root, text="Open Word Document", command=open_file_dialog)
+open_button.pack(pady=10)
+
+# Create a button to clear the result
+clear_button = Button(root, text="Clear Result", command=clear_result)
+clear_button.pack(pady=10)
+
+# Create a label for displaying the result
+result_label = Label(root, text="")
+result_label.pack()
+
+# Start the Tkinter event loop
+root.mainloop()
